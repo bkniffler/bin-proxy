33821969777404e8b58b861a90e4a46f9d00e106
Benjamin Kniffler <benjamin@kniffler.com>
2022-05-11 09:07:21 +0200

    Initial

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { resolve }from 'path';
+import { execSync } from 'child_process';
+
+export default (cmd: string, base: string) => {
+  const { argv } = process;
+
+  const command = `${resolve(base, '../node_modules/.bin', cmd)} ${argv
+    .slice(2)
+    .join(' ')}`;
+  // console.log(`Running <${command}>`);
+  execSync(command, {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+  });
+};
