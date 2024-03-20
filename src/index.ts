12988b8a1b5eecd26b630f8ff6a2c20b5c2bc073
Benjamin Kniffler <benjamin@kniffler.com>
2024-03-20 15:49:42 +0000

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import { resolve }from 'path';
 import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+
+
+export function resolveFirst(paths, throwError = true) {
+  for (const path of paths) {
+    if (existsSync(path)) {
+      return path;
+    }
+  }
+  if (throwError)
+    throw new Error(
+      `Could not find any of the following paths: ${paths.join(', ')}`
+    );
+}
 
 export default (cmd: string, base: string) => {
   const { argv } = process;
 
-  const command = `${resolve(base, '../node_modules/.bin', cmd)} ${argv
+  const bin = resolveFirst([
+    resolve(base, '../node_modules/.bin', cmd),
+    resolve(base, '../../node_modules/.bin', cmd),
+    resolve(base, '../../../node_modules/.bin', cmd),
+    resolve(base, '../../../../node_modules/.bin', cmd),
+  ])
+  const command = `${bin} ${argv
     .slice(2)
     .join(' ')}`;
   // console.log(`Running <${command}>`);
